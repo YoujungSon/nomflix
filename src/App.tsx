08a9a486ac26276a62475ec20e74e6250037c621
YoujungSon <andn1026@gmail.com>
2022-11-21 21:13:02 +0900

    #9.2 Header part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Routes/Home';
 import Search from './Routes/Search';
 import Tv from './Routes/Tv';
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/tv' element={<Tv />} />
         <Route path='/search' element={<Search />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 export default App;
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-
-const Home = () => {
-  return <div style={{ backgroundColor: 'black', height: '200vh' }}></div>;
-};
-
+function Home() {
+  return <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>Home</div>;
+}
 export default Home;
