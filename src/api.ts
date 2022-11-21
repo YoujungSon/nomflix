a036ba9fd6698e79171019db24bd5c0c550d1ff6
YoujungSon <andn1026@gmail.com>
2022-11-22 02:51:34 +0900

    #9.5 Home Screen part One

---
+++ src/api.ts
@@ -0,0 +1,6 @@
+const API_KEY = '7a1b0ea41e9a2e54f71801bc4dc1f6f2';
+const BASE_PATH = 'https://api.themoviedb.org/3/';
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then((response) => response.json());
+}
